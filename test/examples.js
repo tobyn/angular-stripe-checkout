9d775e3c8c2c7c3e3e6e68daeea015e68357fed3
Tobyn Baugher <tobynb@gmail.com>
2017-05-03 12:39:57 -0400

    Update tests to match Stripe Checkout form

--- test/examples.js
+++ test/examples.js
@@ -3,8 +3,8 @@ var assert = require('assert');
 
 var GOT_TOKEN = /Got Stripe token: \S+/;
 
-describe('The simple example',function() {
-  it('returns a Stripe token',function() {
+describe('The simple example', function() {
+  it('returns a Stripe token', function() {
     browser.url('/examples/simple.html');
 
     browser.waitForExist('.stripe_checkout_app');
@@ -13,9 +13,9 @@ describe('The simple example',function() {
     browser.frame('stripe_checkout_app');
     browser.waitForVisible('h1');
 
-    assert.equal(browser.getText('h1'),'Simple Example');
-    assert.equal(browser.getText('h2'),'Five dollahs!');
-    assert.equal(browser.getText('#submitButton'),'Pay $5.00');
+    assert.equal(browser.getText('h1'), 'Simple Example');
+    assert.equal(browser.getText('h2'), 'Five dollahs!');
+    assert.equal(browser.getText('button[type=submit]'), 'Pay $5.00');
 
     fillStripeCheckout();
 
@@ -29,24 +29,24 @@ describe('The simple example',function() {
   });
 });
 
-describe('The custom example',function() {
-  it('returns a Stripe token',function() {
+describe('The custom example', function() {
+  it('returns a Stripe token', function() {
     browser.url('/examples/custom.html');
 
     browser.waitForExist('h1');
-    assert.equal(browser.getText('h1'),'No Stripe Checkout here');
+    assert.equal(browser.getText('h1'), 'No Stripe Checkout here');
     browser.click('a');
 
     browser.waitForExist('button');
-    assert.equal(browser.getText('h1'),'Stripe Checkout has been loaded');
+    assert.equal(browser.getText('h1'), 'Stripe Checkout has been loaded');
     browser.click('button');
 
     browser.frame('stripe_checkout_app');
     browser.waitForVisible('h1');
 
-    assert.equal(browser.getText('h1'),'Custom Example');
-    assert.equal(browser.getText('h2'),'Ten dollahs!');
-    assert.equal(browser.getText('#submitButton'),'Pay $10.00');
+    assert.equal(browser.getText('h1'), 'Custom Example');
+    assert.equal(browser.getText('h2'), 'Ten dollahs!');
+    assert.equal(browser.getText('button[type=submit]'), 'Pay $10.00');
 
     fillStripeCheckout();
 
@@ -62,19 +62,21 @@ describe('The custom example',function() {
 
 
 function fillStripeCheckout() {
-  browser.setValue('#email','test@example.com');
+  browser.setValue(
+    'input[autocomplete=email]',
+    'test-angular-stripe-checkout@example.com');
 
-  browser.addValue('#card_number','4242');
-  browser.addValue('#card_number','4242');
-  browser.addValue('#card_number','4242');
-  browser.addValue('#card_number','4242');
+  browser.addValue('input[autocomplete=cc-number]', '4242');
+  browser.addValue('input[autocomplete=cc-number]', '4242');
+  browser.addValue('input[autocomplete=cc-number]', '4242');
+  browser.addValue('input[autocomplete=cc-number]', '4242');
 
-  browser.addValue('#cc-exp','12');
-  browser.addValue('#cc-exp','60');
+  browser.addValue('input[autocomplete=cc-exp]', '12');
+  browser.addValue('input[autocomplete=cc-exp]', '60');
 
-  browser.setValue('#cc-csc','123');
+  browser.setValue('input[placeholder=CVC]', '123');
 
-  browser.click('#submitButton');
+  browser.click('button[type=submit]');
 }
 
 function waitForTokenAlert() {
@@ -84,5 +86,5 @@ function waitForTokenAlert() {
     } catch (e) {
       return false;
     }
-  },10000,'Expected an alert with the Stripe token');
+  }, 10000, 'Expected an alert with the Stripe token');
 }
